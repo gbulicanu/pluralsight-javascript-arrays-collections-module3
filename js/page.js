f3930d07c97ef825cbe84f58e60696d67a2b95b9
Gheorghe Bulicanu <gbulicanu@gbfsoft.com>
2020-05-25 07:03:09 +0300

    Update the graphs

--- js/page.js
+++ js/page.js
@@ -18,19 +18,28 @@ function addSale() {
   monthlySales.add(parseInt(newAmount.value));
   monthlyLabels.add(newMonth.value);
 
-  for (let total of monthlySales) console.log(total);
-}
-
-function deleteValue() {
-  monthlySales.delete("1500");
-}
-
-function addTotal() {
   yearlyTotal = 0;
+  monthlySalesChart.data.datasets.forEach((dataset) => {
+    dataset.data = [];
+  });
   for (let amount of monthlySales) {
     yearlyTotal += amount;
     yearlyLabel.innerHTML = yearlyTotal;
+
+    monthlySalesChart.data.datasets.forEach((dataset) => {
+      dataset.data.push(amount);
+    });
   }
+
+  monthlySalesChart.data.labels = Array.from(monthlyLabels);
+  monthlySalesChart.update();
+}
+
+function deleteValue() {
+  monthlySales.delete(1500);
+}
+
+function addTotal() {
   // Or
   //   monthlySales.forEach((value1, _value2, _monthlySales) => {
   //     yearlyTotal += value1;
@@ -39,31 +48,35 @@ function addTotal() {
 }
 
 // Bar chart
-// let monthlySalesChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: monthlyLabels,
-//         datasets: [{
-//             label: '# of Sales',
-//             data: monthlySales,
-//             backgroundColor: [
-//                 'rgba(238, 184, 104, 1)',
-//                 'rgba(75, 166, 223, 1)',
-//                 'rgba(239, 118, 122, 1)',
-//             ],
-//             borderWidth: 0
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
+let monthlySalesChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "# of Sales",
+        data: [],
+        backgroundColor: [
+          "rgba(238, 184, 104, 1)",
+          "rgba(75, 166, 223, 1)",
+          "rgba(239, 118, 122, 1)",
+        ],
+        borderWidth: 0,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  },
+});
 
 // // Pie Chart
 // let deptSalesChart = new Chart(pieCtx, {
